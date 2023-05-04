59c0f217112f49d8bc62b6821973f9e2881cdd8f
wangshushu0213 <83213834+wangshushu0213@users.noreply.github.com>
2023-05-04 13:59:05 +0800

    Update and rename readme_decode.sh to decode.sh

--- decode.sh
+++ decode.sh
@@ -1,10 +1,4 @@
 set -e
-# To understand our code:
-# Our two-stage beam search is implemented in fairseq/monotonic_sequence_generator.py
-# We have made detailed code annotation for it. It will be easy for you to understand it.
-# To get mono-kd produced pseudo-ref:
-# S1. Train an offline NMT.
-# S2. Run the mono_kd function below to get the Re-Generated Trainset.
 MODEL=PATH_TO_MODEL
 DATA=PATH_TO_DATA
 PYTHON=PATH_TO_PYTHON
